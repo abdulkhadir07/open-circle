380a877cd07e91a23766cd1a9d92ece630a80374
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-07-20 04:28:23 -0400

    Initialize Spring Boot backend

---
+++ backend/src/test/java/com/opencircle/OpenCircleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCircleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
