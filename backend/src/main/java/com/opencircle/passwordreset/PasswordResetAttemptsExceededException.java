773b77901474b0c3c333de7c4a89e57a9e0a2d3a
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-08-06 03:43:35 -0400

    feat(password-reset): add reset service and password change support

--- backend/src/main/java/com/opencircle/passwordreset/PasswordResetAttemptsExceededException.java
+++ backend/src/main/java/com/opencircle/passwordreset/PasswordResetAttemptsExceededException.java
@@ -1,7 +1,11 @@
 package com.opencircle.passwordreset;
 
-public class PasswordResetAttemptsExceededException extends RuntimeException {
-  public PasswordResetAttemptsExceededException(String message) {
-    super(message);
-  }
+import com.opencircle.common.ApiException;
+import org.springframework.http.HttpStatus;
+
+class PasswordResetAttemptsExceededException extends ApiException {
+
+    PasswordResetAttemptsExceededException() {
+        super(HttpStatus.BAD_REQUEST, "Password reset attempts exceeded. Please request a new code");
+    }
 }
