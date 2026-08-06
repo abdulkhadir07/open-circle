30d6adfe7d693573b4d29c2669b3fdbfd0d19b72
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-08-06 03:43:25 -0400

    refactor(common): share OTP code generator

--- backend/src/test/java/com/opencircle/common/OtpCodeGeneratorTest.java
+++ backend/src/test/java/com/opencircle/common/OtpCodeGeneratorTest.java
@@ -1,4 +1,17 @@
 package com.opencircle.common;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OtpCodeGeneratorTest {
+
+    private final OtpCodeGenerator generator = new OtpCodeGenerator();
+
+    @Test
+    void generateReturnsSixDigitCode() {
+        String code = generator.generate();
+
+        assertThat(code).matches("\\d{6}");
+    }
 }
