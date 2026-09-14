79441894fe81df70f8ca619e9a3d2c3266a673a6
Abdul Khadir <abdulkhadirjallow743@gmail.com>
2026-09-14 12:35:04 -0700

    feat(notification): add transactional notification publisher

---
+++ backend/src/main/java/com/opencircle/notification/NotificationType.java
@@ -0,0 +1,9 @@
+package com.opencircle.notification;
+
+public enum NotificationType {
+    ENGAGEMENT_REQUESTED,
+    ENGAGEMENT_ACCEPTED,
+    ENGAGEMENT_DECLINED,
+    ENGAGEMENT_HELD,
+    ENGAGEMENT_WITHDRAWN
+}
